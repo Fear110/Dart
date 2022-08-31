0f70378e16c4947b2230667bc008154e777c6660
Sakar Pudasaini <barcaboysakar@gmail.com>
2022-08-31 15:20:15 +0545

    Fourth Commit

--- Start.dart
+++ Start.dart
@@ -1,57 +1,103 @@
 import 'dart:io';
 
 void main() {
-  print('Hello, World!');
-  var name;
-  name = "sakar Pudasaini";
-  print(name);
-  int age = 22;
-  print(age);
+//   print('Hello, World!');
+//   var name;
+//   name = "sakar Pudasaini";
+//   print(name);
+//   int age = 22;
+//   print(age);
 
-  print("your name is ${name} and age is ${age}"); // Just like in javaScript
-  // print("Your college: ");
-  // String? college = stdin.readLineSync();
-  // print("Your college is ${college}");
+//   print("your name is ${name} and age is ${age}"); // Just like in javaScript
+//   // print("Your college: ");
+//   // String? college = stdin.readLineSync();
+//   // print("Your college is ${college}");
 
+// //List
+//   List number = [10, 12, 13];
+//   for (var num in number) {
+//     print(num);
+//   }
+//   print("second list");
+//   number.add(14);
+//   number.add(45);
+//   for (var num in number) {
+//     print(num);
+//   }
+//   List name22 = ["sakar", "Fear"];
+//   for (var n in name22) {
+//     print(n);
+//   }
+//   List name2 = ["kakashi", "Balrog"];
+//   List nameFinal = [
+//     ...name22,
+//     ...name2
+//   ]; // Addting two different Lists or array
+//   for (var nameher in nameFinal) {
+//     print(nameher);
+//   }
+//   print(nameFinal.length); //Length of the List
+//some method of list
+//   var num = [1, 2, 56,3,45,800];
+//  num.sort();
+//   print(num);
 
+  // var gfg = ['Geeks', 'Geeks', 'sakar', 'fear', 'kakashi'];
+  // print(gfg.lastIndexOf("fear"));
+//   //map in dart
+//   Map namehere = {"sakar": "first", "kakashi": "second"};
 
-//List
-  List number = [10, 12, 13];
-  for (var num in number) {
-    print(num);
-  }
-  print("second list");
-  number.add(14);
-  number.add(45);
-  for (var num in number) {
-    print(num);
-  }
-  List name22 = ["sakar", "Fear"];
-  for (var n in name22) {
-    print(n);
-  }
-  List name2 = ["kakashi", "Balrog"];
-  List nameFinal = [
-    ...name22,
-    ...name2
-  ]; // Addting two different Lists or array
-  for (var nameher in nameFinal) {
-    print(nameher);
-  }
-  print(nameFinal.length); //Length of the List 
+//   for (var n in namehere.keys) {
+//     print("Related values are ${n} and pair value is ${namehere[n]}");
+//   }
 
+//   // Var keyword
+//   var myname = "sakar";
+//   var myage = 12;
+//   print("${myname} ${myage}");
 
+//   var aaa = ["sakar"];
+//   aaa.add("hello");
+//   aaa.add("kakasjoi");
 
-  //map in dart
-    Map namehere = {"sakar": "first", "kakashi": "second"};
+//   for (var tt in aaa) {
+//     print(tt);
+//   }
 
-  for (var n in namehere.keys) {
-    print("Related values are ${n} and pair value is ${namehere[n]}");
-  }
+  // //somebasic operation
+  // var num1 = 12;
+  // var num2 = 10;
 
-  // Var keyword
-   var myname = "sakar";
-  var myage = 12;
-  print("${myname} ${myage}");
+  // var add = num1 + num2;
+  // var sub = num1 - num2;
+  // var mul = num1 * num2;
+  // var mod = num1 % num2;
+  // var div = num1 / num2;
 
+  // print(
+  //     "add is ${add} sub is ${sub} mul is ${mul} mod is ${mod} and div is ${div} ");
+  // If else statement
+
+  // var age = 20;
+  // if (age <= 18) {
+  //   print("You can not vote because you're just ${age}");
+  // } else {
+  //   print("You can vote because you're already ${age}");
+  // }
+  // var marks = 67;
+  // if (marks > 90) {
+  //   print("You got A+");
+  // } else if (marks > 80 && marks < 90) {
+  //   print("You got A");
+  // } else if (marks > 70 && marks < 80) {
+  //   print("You got B+");
+  // }
+  // else if (marks > 60 && marks < 70) {
+  //   print("You got B");
+  // }
+  // else if (marks > 60 && marks < 70) {
+  //   print("You got C");
+  // }
+  // var name = {"kakashi": 1, "sakar": 2};
+  // print(name["sakar"]);
 }
