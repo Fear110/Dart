a3b2da3621c391ce5683c8bab25ec3a8f4f816f9
Sakar Pudasaini <barcaboysakar@gmail.com>
2022-08-31 13:33:41 +0545

    Second Commit

--- Start.dart
+++ Start.dart
@@ -3,4 +3,8 @@ void main() {
   String name;
   name = "sakar Pudasaini";
   print(name);
+  int age = 22;
+  print(age);
+
+  print("your name is ${name} and age is ${age}"); // Just like in javaScript
 }
